97615fecad358e154961e63f82973e2f579e9911
Qiao Shilei <3489150347@qq.com>
2026-06-02 21:47:40 +0800

    完善前端业务功能

--- demo/inventory-frontend/vue.config.js
+++ demo/inventory-frontend/vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: '仓库进销存管理系统',
+    },
+  },
   devServer: {
     port: 8081 // 强制前端使用 8081 端口
   }
